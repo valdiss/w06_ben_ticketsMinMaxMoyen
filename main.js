040e037247acd04957ff925748a38087738bc45d
valdiss <valentin.dissais@gmail.com>
2016-12-15 17:15:42 +0100

    tr en rouge

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
-//  ____       _  __       _____                     _   _             
-// / ___|  ___| |/ _|     | ____|_  _____  ___ _   _| |_(_)_ __   __ _ 
+//  ____       _  __       _____                     _   _
+// / ___|  ___| |/ _|     | ____|_  _____  ___ _   _| |_(_)_ __   __ _
 // \___ \ / _ \ | |_ _____|  _| \ \/ / _ \/ __| | | | __| | '_ \ / _` |
 //  ___) |  __/ |  _|_____| |___ >  <  __/ (__| |_| | |_| | | | | (_| |
 // |____/ \___|_|_|       |_____/_/\_\___|\___|\__,_|\__|_|_| |_|\__, |
-//                                                               |___/ 
-//     _                                                      
-//    / \   _ __   ___  _ __  _   _ _ __ ___   ___  _   _ ___ 
+//                                                               |___/
+//     _
+//    / \   _ __   ___  _ __  _   _ _ __ ___   ___  _   _ ___
 //   / _ \ | '_ \ / _ \| '_ \| | | | '_ ` _ \ / _ \| | | / __|
 //  / ___ \| | | | (_) | | | | |_| | | | | | | (_) | |_| \__ \
 // /_/   \_\_| |_|\___/|_| |_|\__, |_| |_| |_|\___/ \__,_|___/
-//                            |___/                           
-//  _____                 _   _             
-// |  ___|   _ _ __   ___| |_(_) ___  _ __  
-// | |_ | | | | '_ \ / __| __| |/ _ \| '_ \ 
+//                            |___/
+//  _____                 _   _
+// |  ___|   _ _ __   ___| |_(_) ___  _ __
+// | |_ | | | | '_ \ / __| __| |/ _ \| '_ \
 // |  _|| |_| | | | | (__| |_| | (_) | | | |
 // |_|   \__,_|_| |_|\___|\__|_|\___/|_| |_|
 //
 // See http://esbueno.noahstokes.com/post/77292606977/self-executing-anonymous-functions-or-how-to-write
 (function() {
-    
-    
-    
-    
-    //  _   _ _   _ _ _ _           __              _   _             
+
+
+
+
+    //  _   _ _   _ _ _ _           __              _   _
     // | | | | |_(_) (_) |_ _  _   / _|_  _ _ _  __| |_(_)___ _ _  ___
     // | |_| |  _| | | |  _| || | |  _| || | ' \/ _|  _| / _ \ ' \(_-<
     //  \___/ \__|_|_|_|\__|\_, | |_|  \_,_|_||_\__|\__|_\___/_||_/__/
-    //                      |__/                                      
-    // 
+    //                      |__/
+    //
 
-    //  _                                  
-    // |_)   o| _|  _| _._|_ _.|_  _. _ _  
+    //  _
+    // |_)   o| _|  _| _._|_ _.|_  _. _ _
     // |_)|_|||(_| (_|(_| |_(_||_)(_|_>(/_
     //
     // INIT function : fills up a customers orders list
@@ -41,11 +41,11 @@
 
         // Customers list ; will be picked randomly later
         var listeClients = [
-            {name:'Client 1',code:'client1'}, 
-            {name:'Client 2',code:'client2'}, 
-            {name:'Client 3',code:'client3'}, 
-            {name:'Client 2',code:'client4'},   // Set duplicate name on purpose
-            {name:'Client 5',code:'client5'}, 
+            {name:'Client 1',code:'client1'},
+            {name:'Client 2',code:'client2'},
+            {name:'Client 3',code:'client3'},
+            {name:'Client 4',code:'client4'},   // Set duplicate name on purpose
+            {name:'Client 5',code:'client5'},
             {name:'Client 6',code:'client6'}
         ] ;
 
@@ -54,17 +54,17 @@
 
         // Declare our variable that will handle our 'database'
         let listeCdesClients = [] ;
-        
+
         // Let's build 1000 lines
         for(let i=0; i<1000; i++) {
-            
+
             // Pick a random element from customers and vat rates lists
             let idxClient   = Math.round(Math.random()*(listeClients.length - 1)) ;
             let idxTva      = Math.round(Math.random()*(tauxTva.length - 1)) ;
-            
+
             // Compute random price between 0 and 1000
             let randomPrice = Math.random() * 1000 ;
-            
+
             // Compute the order line we are about to add to our 'database'
             // our line is an array
             let orderLine = [
@@ -77,18 +77,18 @@
             // Push these random values to our 'database'
             listeCdesClients.push(orderLine) ;
         }
-        
+
         // Finally, return our 'database'
         return listeCdesClients ;
     } ;
     // Note : semi-colon is needed at line above because we are DECLARING A VARIABLE
-    
-    
-    
-    //                                          
-    //  /\  _| _| |o._  _  _|_ _  _|_ _.|_ | _  
-    // /--\(_|(_| ||| |(/_  |_(_)  |_(_||_)|(/_ 
-    //                                              
+
+
+
+    //
+    //  /\  _| _| |o._  _  _|_ _  _|_ _.|_ | _
+    // /--\(_|(_| ||| |(/_  |_(_)  |_(_||_)|(/_
+    //
     // GUI function : adds a line to customers list table
     // INPUT :  (string) talbeId the html id of the table to add the line to
     //          (array) the table data ; must be same lenght than html static table columns
@@ -97,43 +97,43 @@
     //                  lineData must not be empty, otherwise returns false
     // INTERNALS : adds a DOM tr element to given table ; one td per array field
     var fn_addTableLine = function(tableId, lineData){
-        
+
         // Check that lineData exists and is not empty
         if(!lineData || !lineData.length) return false ;
- 
+
         // Check that given table is actually a DOM table
         let targetTable = document.getElementById(tableId) ;
         if(!targetTable) return false ;
-        
+
         // Still there ? Ok, let's move on...
         console.log(targetTable) ;
-        
+
         // Create an empty <tr> element and add it to the 1st position of the table:
         let row = targetTable.insertRow(0);
-        
+
         // Now, insert as many cells as in our input lineData array
         // for( val IN obj) syntax uses INDEXES of obj, with pointer val
         for(cellIndex in lineData) {
             // The variable cellIndex handles the current lineData's element's index
-            // The value of cellIndex is equivalent to the variable i you may use 
+            // The value of cellIndex is equivalent to the variable i you may use
             // in a for loop with i++ syntax
-            
+
             // Insert new cell (<td> element) at the nth position of the "new" <tr> element:
             let addedCell = row.insertCell(cellIndex);
-            
+
             // Add some text to the new cell:
             addedCell.innerHTML = lineData[cellIndex];
         }
-        
+
         // Return true so that caller function knows everything went well
         return true ;
     } ;
     // Note : semi-colon is needed at line above because we are DECLARING A VARIABLE
-    
-    
-    
-    
-    //  _                                                                      
+
+
+
+
+    //  _
     // |_) _. ._ _  _     _|  _. _|_  _.    _. ._   _|    _  _  | |  _   _ _|_
     // |  (_| | _> (/_   (_| (_|  |_ (_|   (_| | | (_|   (_ (_) | | (/_ (_  |_
     //
@@ -142,12 +142,12 @@
 
         // Check that we actually got a DB to parse (exists and not empty)
         if(!customersOrdersDB || !customersOrdersDB.length) return false ;
-        
+
         // Declare our collector
         // Will be as such : { 'clientCode1' : [ minPriceSeen, MaxPriceSeen], 'clientCode2' : [ ... ], ... }
         let _customersCollector = {} ;
-        
-        // Loop over our database 
+
+        // Loop over our database
         for(orderLine of customersOrdersDB) {
             // The variable orderLine above contains an array,
             // wich in turn contains our customer order data
@@ -155,23 +155,23 @@
             let orderAmount     = orderLine[1] ;
             let orderVatRate    = orderLine[2] ;
             let clientCode      = orderLine[3] ;
-            
+
             // Compute transaction real amount
             // TTC = HT * ( 1 + TVAcoeff )
             // avec TVAcoeff = TVApercent / 100
             let orderTtcAmount  = orderAmount * ( 1 + orderVatRate/100 ) ;
-                
+
             // _________________________
             // DO STATISTICAL PROCESSING
             // Collect data
-                
+
             // Check wether this client has been seen already
             // NOTE : use clientCode as key, because several customers may have the same name
             if(_customersCollector[clientCode]) {
                 // This customer has been seen already :
                 // We must check current amount against already seen minimum and
                 // maximum, and eventually adjust one of them
-                
+
                 // Check minimum value
                 if(_customersCollector[clientCode][1] > orderTtcAmount) {
                     // We have a new minimum value : update it in collector
@@ -191,39 +191,39 @@
                 _customersCollector[clientCode] = [ clientName, orderTtcAmount , orderTtcAmount ] ;
             }
             // _________________________
-            
-            
+
+
         }
-        
+
         // Finally, return our collector for further processing
         return _customersCollector ;
-        
+
     } ;
     // Note : semi-colon is needed at line above because we are DECLARING A VARIABLE
-    
-    
-    
-    
-    //  ___         _      _     _      _ _   
-    // / __| __ _ _(_)_ __| |_  (_)_ _ (_) |_ 
+
+
+
+
+    //  ___         _      _     _      _ _
+    // / __| __ _ _(_)_ __| |_  (_)_ _ (_) |_
     // \__ \/ _| '_| | '_ \  _| | | ' \| |  _|
     // |___/\__|_| |_| .__/\__| |_|_||_|_|\__|
-    //               |_|                          
-    
+    //               |_|
+
     // Actually launch engine
 
     // First, get some data to play with
     // NOTE : this is synchronous, we can expect our DB to be crafted
     // when next instruction comes to be executed
     let customersOrdersDB       = fn_init() ;
-    
+
     // Then, parse ou DB and extract stat values
     let customersOrdersStats    = fn_parseAndExtract(customersOrdersDB) ;
     console.log(Object.keys(customersOrdersStats)) ;
-    
+
     // And, add lines to our table so that we can see the result
     if(customersOrdersStats && Object.keys(customersOrdersStats).length) {
-        
+
         // We actually have data to play with : we can move on ...
         // Loop over our collector, and add a line per customer
         // NOTE : you must have as many lines in your table
@@ -234,16 +234,20 @@
             let clientName  = customersOrdersStats[clientCode][0] ;
             let minAmount   = customersOrdersStats[clientCode][1] ;
             let maxAmount   = customersOrdersStats[clientCode][2] ;
-            
+
             // Craft array to be passed to function fn_addTableLine
             let lineToAddToGui = [clientName, clientCode, minAmount, maxAmount] ;
-            
+
             // and actually add it
             fn_addTableLine('SM3customersStatsTable',lineToAddToGui) ;
         }
     }
-   
-    
-    
+    let redFill= function(){
+      document.querySelectorAll('td:nth-child('+ 3 +')').forEach(function(el){
+                if (el.innerHTML<5)
+                el.parentNode.classList.add('red');
+            })
+    };
+    redFill();
 
 })() ;
